0c628a053e32522c2081626c91b3719c3f0bbfe2
beryld <deback.beryl@gmail.com>
2022-02-24 10:35:22 +0100

    Feat : add tva and total calculation functions

--- basketclasses.php
+++ basketclasses.php
@@ -15,11 +15,12 @@ class Product
     }
     public function getTotal()
     {
+        return ($this->price * $this->amount);
         
     }
     public function getTva()
     {
-
+        return ($this->gettotal()/100* $this->tva);
     }
 }
 
@@ -27,5 +28,8 @@ class Product
 $product1 = new Product ('banana', 1, 6, 6);
 $product2 = new Product ('apple', 1.5, 3, 6);
 $product3 = new Product ('wine', 10, 2, 6);
+echo $product2 ->getTotal();
+echo $product2-> getTva();
+// echo getTotal()
 //Could have created this with a foreach from basket.php
 

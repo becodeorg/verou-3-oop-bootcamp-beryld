8a85e167095ec1fff6b9ec65d0e42af3031fb60e
beryld <deback.beryl@gmail.com>
2022-02-24 10:40:25 +0100

    Feat : add use case 1 completion

--- basketclasses.php
+++ basketclasses.php
@@ -28,8 +28,16 @@ class Product
 $product1 = new Product ('banana', 1, 6, 6);
 $product2 = new Product ('apple', 1.5, 3, 6);
 $product3 = new Product ('wine', 10, 2, 6);
+echo ($product2 ->getTotal()) + ($product1 ->getTotal())+ ($product3->getTotal());
+echo "<br>";
+echo $product1 -> getTotal();
+echo "<br>";
 echo $product2 ->getTotal();
+echo "<br>";
+echo $product3 -> getTotal();
+echo "<br>";
 echo $product2-> getTva();
+
 // echo getTotal()
 //Could have created this with a foreach from basket.php
 

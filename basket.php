fdf57fe3ed945fb106edad02c5bc9844782af3ef
beryld <deback.beryl@gmail.com>
2022-02-24 10:02:10 +0100

    Feat : add the tva calculation

--- basket.php
+++ basket.php
@@ -7,19 +7,30 @@ error_reporting(E_ALL);
 
 $products = 
 [
-    ["name" => 'banana', 'price' => 1, 'pieces' => 6],
-    ["name" => 'apple', 'price' => 1.5, 'pieces' => 3],
-    ["name" => 'wine', 'price' => 10, 'pieces' => 2]
+    ['name' => 'banana', 'price' => 1, 'pieces' => 6, 'tva' => 6],
+    ['name' => 'apple', 'price' => 1.5, 'pieces' => 3, 'tva' => 6],
+    ['name' => 'wine', 'price' => 10, 'pieces' => 2, 'tva' => 6]
 ];
             
 $total = 0;            
 $calculEdit = 0;
+$tvapart = 0;
+$tvatotal = 0;
 
 foreach($products as $index => $calculatePrice)
 {
+    //Calcul the total price tva included
     $calcul = (($products[$index]['price']) * ($products[$index]['pieces']));
     $calculEdit = $calculEdit + $calcul;
-   
+    
+   //Calcul the tva percentage
+   if($products[$index]['tva']==6)
+   {
+        $tvapart = (($calcul / 100) * 6);
+        $name = $products[$index]["name"];
+        echo "$tvapart is the tva part of  $name ";
+        echo "<br>";
+    }
 }
 
 echo "the total price is :  $calculEdit";

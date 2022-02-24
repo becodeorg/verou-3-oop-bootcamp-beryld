a75228670725844f66af07a7540a502950882492
beryld <deback.beryl@gmail.com>
2022-02-24 15:14:05 +0100

    Edit : Create usecase3 and add students

---
+++ usecase3/students.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+// if class_exits && score >20 then change class to 1 (classexists or class=1 ?)
+
+// Name, grade class 
+
+$student1 = new Student ('Hydrogen', 1.008);
+$student2 = new Student ('Helium', 4.0026);
+$student3 = new Student ('Lithium', 6.94);
+$student4 = new Student ('Beryllium', 9.0122);
+$student5 = new Student ('Boron', 10.81);
+$student6 = new Student ('Carbon', 12.011);
+$student7 = new Student ('Nitrogen', 14.007);
+$student8 = new Student ('Oxygen', 15.999);
+$student9 = new Student ('Fluorine', 18.998);
+$student10 = new Student ('Neon', 20.180);
+$student11 = new Student ('Sodium', 22.990);
+$student12 = new Student ('Magnesium',24.305);
+$student13 = new Student('Aluminium', 26.982);
+$student14 = new Student ('Silicon', 28.085);
+$student15 = new Student ('Phosphorus',30.974);
+$student16 = new Student('Sulphur', 32.06);
+$student17 = new Student('Chlorine', 35.45);
+$student18 = new Student('Argon',39.95);
+$student19 = new Student ('Potassium', 39.098);
+$student20 = new Student ('Calcium',40.078);
